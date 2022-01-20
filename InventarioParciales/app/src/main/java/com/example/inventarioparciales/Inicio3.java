18a9e4e7243605e348981d71e6c82b82e3f8ab97
armandoPineda <armandopineda@wposs.com>
2022-01-20 16:38:28 -0500

    IMPLEMENTACION - HOME
    *) se implementa el diseño del home
    *) se agregan dependencias en el builgradel
    *) se agrega inten botom invitado

--- InventarioParciales/app/src/main/java/com/example/inventarioparciales/Inicio3.java
+++ InventarioParciales/app/src/main/java/com/example/inventarioparciales/Inicio3.java
@@ -10,17 +10,28 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class Inicio3 extends AppCompatActivity {
-Button btnRegister;
+Button btnRegister,btnInvited;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio3);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
      
         btnRegister=findViewById(R.id.btnRegister);
+        btnInvited=findViewById(R.id.btnInvited);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Inicio3.this, Register.class);
+                startActivity(i);
+            }
+        });
+
+        btnInvited.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Inicio3.this, Home.class);
                 startActivity(i);
             }
         });
