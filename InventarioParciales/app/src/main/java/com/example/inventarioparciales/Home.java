e5827c2ec1a76caaa980428355e72294f27a08d8
armandoPineda <armandopineda@wposs.com>
2022-01-21 08:58:44 -0500

    Revert "IMPLEMENTACION - HOME"
    This reverts commit 18a9e4e7243605e348981d71e6c82b82e3f8ab97.

--- InventarioParciales/app/src/main/java/com/example/inventarioparciales/Home.java
+++ InventarioParciales/app/src/main/java/com/example/inventarioparciales/Home.java
@@ -1,64 +1,14 @@
 package com.example.inventarioparciales;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.ViewPager2;
-import android.graphics.drawable.Drawable;
+
 import android.os.Bundle;
-import android.widget.LinearLayout;
-import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
-
-    ArrayList<MateriasHome> listHome;
-    RecyclerView recyclerView;
-    LinearLayout dotslayout;
-    SliderAdapter adapter;
-    ViewPager2 pager2;
-    Drawable list[];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        listHome = new ArrayList<>();
-        recyclerView = findViewById(R.id.RecyclerId);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
-        dotslayout = findViewById(R.id.containerPuntos);
-        pager2 = findViewById(R.id.viewPager2);
-        list = new Drawable[3];
-        list[0] = getResources().getDrawable(R.drawable.bannerperfecto);
-        list[1] = getResources().getDrawable(R.drawable.bannerperfecto2);
-        list[2] = getResources().getDrawable(R.drawable.bannerperfecto3);
-
-        adapter = new SliderAdapter(list);
-        pager2.setAdapter(adapter);
-        pager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-            }
-        });
-
-        llenarPersonajes();
-        AdapterHome adapterHome = new AdapterHome(listHome);
-        recyclerView.setAdapter(adapterHome);
-    }
-
-
-    private void llenarPersonajes() {
-        listHome.add(new MateriasHome("Base De Datos",R.drawable.dime));
-        listHome.add(new MateriasHome("Ingenieria De Software",R.drawable.softwa));
-        listHome.add(new MateriasHome("Programacion I",R.drawable.python));
-        listHome.add(new MateriasHome("Programacion II",R.drawable.python));
-        listHome.add(new MateriasHome("Estructura De Datos",R.drawable.graphm));
-        listHome.add(new MateriasHome("Redes y Computadores",R.drawable.network));
-        listHome.add(new MateriasHome("Inteligencia Artificial",R.drawable.robot));
-        listHome.add(new MateriasHome("Tecnicas De Investigacion",R.drawable.tecnica));
-        listHome.add(new MateriasHome("Proyectos I",R.drawable.proyec1));
-
-
     }
 }
--- InventarioParciales/app/src/main/java/com/example/inventarioparciales/Inicio3.java
+++ InventarioParciales/app/src/main/java/com/example/inventarioparciales/Inicio3.java
@@ -10,28 +10,17 @@ import android.view.WindowManager;
 import android.widget.Button;
 
 public class Inicio3 extends AppCompatActivity {
-Button btnRegister,btnInvited;
+Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio3);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
      
         btnRegister=findViewById(R.id.btnRegister);
-        btnInvited=findViewById(R.id.btnInvited);
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(Inicio3.this, Register.class);
-                startActivity(i);
-            }
-        });
-
-        btnInvited.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Inicio3.this, Home.class);
                 startActivity(i);
             }
         });
